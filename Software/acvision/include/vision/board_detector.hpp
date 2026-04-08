8a3e3cd2aa7e94f7fb28878a5e3ad40e211cdd33
Moreno Jones <d24307@academico.domhelder.edu.br>
2026-04-07 23:12:27 -0300

    Document BoardCorners structure
    Added documentation for BoardCorners structure.

--- Software/acvision/include/vision/board_detector.hpp
+++ Software/acvision/include/vision/board_detector.hpp
@@ -5,7 +5,15 @@
 #include <optional>
 
 namespace ac {
-
+/**
+ * @brief Representa os quatro cantos externos estimados do tabuleiro.
+ *
+ * A ordem esperada dos pontos é:
+ * 0 - top-left
+ * 1 - top-right
+ * 2 - bottom-right
+ * 3 - bottom-left
+ */
 struct BoardCorners {
     std::array<cv::Point2f, 4> corners;
 };
